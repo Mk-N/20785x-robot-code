a023c94934197d8aa9e96f0a89c71cca4d7c7f07
Mk-N <Reachtomukunth@gmail.com>
2023-04-10 17:36:30 +0100

    Added sylib
    Included the sylib function in the code

--- PROS_Flywheel_pidv4.0print/include/main.h
+++ PROS_Flywheel_pidv4.0print/include/main.h
@@ -4,7 +4,7 @@
  * Contains common definitions and header files used throughout your PROS
  * project.
  *
- * \copyright Copyright (c) 2017-2023, Purdue University ACM SIGBots.
+ * Copyright (c) 2017-2022, Purdue University ACM SIGBots.
  * All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
@@ -22,7 +22,7 @@
  *
  * For instance, E_CONTROLLER_MASTER has a shorter name: CONTROLLER_MASTER.
  * E_CONTROLLER_MASTER is pedantically correct within the PROS styleguide, but
- * not convenient for most student programmers.
+ * not convienent for most student programmers.
  */
 #define PROS_USE_SIMPLE_NAMES
 
@@ -35,11 +35,12 @@
 #define PROS_USE_LITERALS
 
 #include "api.h"
+#include "sylib/sylib.hpp"
 
 /**
  * You should add more #includes here
  */
-#include "okapi/api.hpp"
+//#include "okapi/api.hpp"
 //#include "pros/api_legacy.h"
 
 /**
@@ -75,10 +76,9 @@ void opcontrol(void);
 /**
  * You can add C++-only headers here
  */
-#include <queue>
+//#include <iostream>
 #include <sstream>
 #include <fstream>
-
 #endif
 
 #endif  // _PROS_MAIN_H_
