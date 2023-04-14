7959e5345e25fd53f6376ff67e7cec7d5ab66290
Mk-N <126353403+Mk-N@users.noreply.github.com>
2023-04-14 06:28:50 +0100

    Update main.cpp
    made on_center_button easier to read

--- PROS_Flywheel_pidv4.1print/src/main.cpp
+++ PROS_Flywheel_pidv4.1print/src/main.cpp
@@ -17,9 +17,12 @@ void on_center_button()
 {
 	static bool pressed = false;
 	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
+	if (pressed)
+	{
+  	pros::lcd::set_text(2, "I was pressed!");
+	}
+	else
+	{
 		pros::lcd::clear_line(2);
 	}
 }
